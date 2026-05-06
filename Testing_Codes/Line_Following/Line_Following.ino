97da9f65fa51a954cbfbb1e287ea9d5a82c62e09
RobotsAndAryan <aryannaik1205@gmail.com>
2026-05-06 11:10:46 +0100

    line following and navigating improved

--- Testing_Codes/Line_Following/Line_Following.ino
+++ Testing_Codes/Line_Following/Line_Following.ino
@@ -6,59 +6,51 @@ MotoronI2C mc;
 int SensorPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 int SensorCount = 9;
 
-// Define both control pins
 const uint8_t emitterPinOdd = 11;  
 const uint8_t emitterPinEven = 12; 
 
 int spd = 500;
-int direction=0;
 
-void navigation_and_movement(int direction){
-  switch (direction){
-    case 1: // Move forward
-      mc.setSpeed(1,spd); 
-      mc.setSpeed(2,spd);
+void navigation_and_movement(char dir){
+  switch (dir){
+    case 'w': 
+      mc.setSpeed(1, spd); 
+      mc.setSpeed(2, spd);
       break;
-    case 2: // Move backward
-      mc.setSpeed(1,-spd);
+    case 's': 
+      mc.setSpeed(1, -spd);
       mc.setSpeed(2, -spd);
       break;
-    case 3: // Turn Left
-      mc.setSpeed(1, spd-100);
-      mc.setSpeed(2,spd+100);
+    case 'a': 
+      mc.setSpeed(1, spd - 100);
+      mc.setSpeed(2, spd + 100);
       break;
-    case 4: // Turn right
-      mc.setSpeed(1, spd+100);
-      mc.setSpeed(2,spd-100);
-    default: // Stay Still
-      mc.setSpeed(1,0);
-      mc.setSpeed(2,0);
+    case 'd': 
+      mc.setSpeed(1, spd + 100);
+      mc.setSpeed(2, spd - 100);
+      break; 
+    default: 
+      mc.setSpeed(1, 0);
+      mc.setSpeed(2, 0);
   }
 }
 
-void line_detecting(){
-  uint16_t sensorValues[SensorCount];
-
-  // 1. Charge the capacitors on the sensor board
+void line_detecting(uint16_t* sensorValues){
   for (uint8_t i = 0; i < SensorCount; i++) {
     pinMode(SensorPins[i], OUTPUT);
     digitalWrite(SensorPins[i], HIGH);
   }
 
-  // Wait 15 microseconds for capacitors to fully charge
   delayMicroseconds(15);
 
-  // 2. Switch pins to INPUT to let them discharge
   for (uint8_t i = 0; i < SensorCount; i++) {
     pinMode(SensorPins[i], INPUT);
-    sensorValues[i] = 1000; // Default to max timeout of 1000
+    sensorValues[i] = 1000; 
   }
 
-  // 3. Measure discharge time WITHOUT disabling system interrupts
   unsigned long startTime = micros();
-  while (micros() - startTime < 1000) { // 1000us absolute max timeout
+  while (micros() - startTime < 1000) { 
     for (uint8_t i = 0; i < SensorCount; i++) {
-      // If the pin drops to LOW, record how long it took
       if (sensorValues[i] == 1000 && digitalRead(SensorPins[i]) == LOW) {
         sensorValues[i] = micros() - startTime;
       }
@@ -66,6 +58,28 @@ void line_detecting(){
   }
 }
 
+void line_calibration(){
+  uint16_t max_time[9] = {0};
+  uint16_t min_time[9] = {1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000};
+  uint16_t line_data[9];
+
+  delay(1000);
+  Serial.println("Line Calibration Time , slide the robot accross the line");
+  
+  unsigned long time_start = millis();
+  while((millis() - time_start) < 3000) {
+    line_detecting(line_data);
+    for (int i = 0; i < SensorCount; i++) {
+      if (line_data[i] > max_time[i]) max_time[i] = line_data[i];
+      if (line_data[i] < min_time[i]) min_time[i] = line_data[i];
+    }
+  }
+  Serial.println("calibration done");
+}
+
+void line_following_and_motor_correction(){
+  
+}
 
 void setup(){
   delay(2000);
@@ -74,30 +88,32 @@ void setup(){
   Wire.begin();
   Wire1.begin();
 
+  mc.setBus(&Wire1);
   mc.setAddress(0x14);
-  mc.setMaxDeceleration(1,400);
-  mc.setMaxDeceleration(2,400);
 
-  mc.setMaxAcceleration(1,400);
-  mc.setMaxAcceleration(2,400);
+  mc.reinitialize();
+  mc.clearResetFlag();
+  mc.disableCommandTimeout();
 
-  mc.setPwmMode(1,6);
-  mc.setPwmMode(2,6);
+  mc.setMaxDeceleration(1, 400);
+  mc.setMaxDeceleration(2, 400);
+  mc.setMaxAcceleration(1, 400);
+  mc.setMaxAcceleration(2, 400);
 
-  // 2. Turn on ALL IR LEDs (Both Odd and Even banks)
+  mc.setPwmMode(1, 6);
+  mc.setPwmMode(2, 6);
+
   pinMode(emitterPinOdd, OUTPUT);
   pinMode(emitterPinEven, OUTPUT);
   digitalWrite(emitterPinOdd, HIGH); 
   digitalWrite(emitterPinEven, HIGH); 
-
-};
+}
 
 void loop(){
-  if (Serial.available()!=0){
-    if(Serial.read()!=0){
-      direction = Serial.read();
-      navigation_and_movement(direction);
+  if (Serial.available() > 0){
+    char incoming = Serial.read();
+    if(incoming != '\n' && incoming != '\r') {
+      navigation_and_movement(incoming);
     }
   }
-
-};
+}
