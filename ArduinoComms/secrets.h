e24a169d82b8fcfc8ef643ceb0167e54c943bff6
georgersamuelson <george.r.samuelson@gmail.com>
2026-05-19 10:43:16 +0100

    Update secrets.h
    Provided actual wifi credentials

--- ArduinoComms/secrets.h
+++ ArduinoComms/secrets.h
@@ -1,5 +1,5 @@
-#define WIFI_SSID "InnovationLab_WiFi"
-#define WIFI_PASSWORD "WiFi_password"
+#define WIFI_SSID "PhaseSpaceNetwork_2.4G"
+#define WIFI_PASSWORD "8igMacNet"
 #define BROKER_HOST "192.168.0.74" // Lab MQTT Broker
 #define BROKER_PORT 1883
 #define GROUP_ID "13"  // Use your assigned team number
